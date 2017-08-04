dc1ac26f1833b1e4b9c0f53d8b91b6814bf9d75f
Jeff Becker <ampernand@gmail.com>
2017-08-04 13:22:01 -0400

    null check

--- src/crypto/openssl/elgamal.c
+++ src/crypto/openssl/elgamal.c
@@ -42,7 +42,8 @@ void i2p_openssl_elg_encrypt(struct i2p_crypto * crypto, struct elg_op * op)
 
   BN_CTX_free(ctx);
 
-  op->result(op);
+  if(op->result)
+    op->result(op);
 }
 
 void i2p_openssl_elg_decrypt(struct i2p_crypto * crypto, struct elg_op * op)
