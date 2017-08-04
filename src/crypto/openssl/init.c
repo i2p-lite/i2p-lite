8b40db2ff11d5b162380c03fc8a7cc3f3acf0bb3
Jeff Becker <ampernand@gmail.com>
2017-08-04 11:03:05 -0400

    more

--- src/crypto/openssl/init.c
+++ src/crypto/openssl/init.c
@@ -1,5 +1,5 @@
 #include <i2p/cryptoimpl/openssl.h>
-
+#include <string.h>
 
 struct i2p_crypto_openssl_st
 {
@@ -12,7 +12,8 @@ void i2p_crypto_openssl_singlethreaded(struct i2p_crypto * crypto, struct i2p_al
   impl = alloc->alloc(alloc, sizeof(struct i2p_crypto_openssl_st));
   impl->alloc = alloc;
 
-
+  /* zero out function pointers */
+  memset(crypto, 0, sizeof(struct i2p_crypto));
 
   crypto->impl = impl;
 }
